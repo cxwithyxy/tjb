1856604f25c596f146f70cec39d6a814675c675f
cx2889 <523764720@qq.com>
2019-04-10 01:01:39 +0800

    好像可以登陆了

--- index.ts
+++ index.ts
@@ -1,7 +1,17 @@
 const { app, BrowserWindow } = require('electron')
 
 class tjb {
-    constructor() {
+
+    public app = null;
+
+    constructor(app: any) {
+        this.app = app;
+
+        this.path_handle();
+        this.goto_tb_page();
+    }
+
+    goto_tb_page() {
         let win = new BrowserWindow({ 
             width: 480,
             height: 800,
@@ -12,10 +22,17 @@ class tjb {
             }
         });
         let wincc = win.webContents;
-        wincc.setUserAgent("User-Agent:	Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Mobile/15E148 Safari/604.1")
-        win.loadURL("https://market.m.taobao.com/apps/market/tjb/core-member2.html");
-        // wincc.loadURL("https://echo.opera.com");
+        wincc.setUserAgent("User-Agent:	Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Mobile/15E148 Safari/604.1");
+        wincc.loadURL("https://market.m.taobao.com/apps/market/tjb/core-member2.html");
+        // wincc.loadURL("https://m.baidu.com");
+        // setTimeout(() => {
+        //     wincc.enableDeviceEmulation({});
+        // }, 1500);
+    }
+
+    path_handle() {
+        app.setPath("userData", app.getPath("temp") + "/tjb");
     }
 }
 
-app.on('ready', ()=>{new tjb()})
+app.on('ready', ()=>{new tjb(app)})
