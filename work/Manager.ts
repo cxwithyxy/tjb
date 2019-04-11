59f86d355033ddc4c14eb77e2f5e33efec3d2b63
chenxuan <523764720@qq.com>
2019-04-11 11:43:12 +0800

    优化manager.ts的代码规范

--- work/Manager.ts
+++ work/Manager.ts
@@ -1,12 +1,17 @@
-const { Worker } = require("./Worker")
-const { _ } = require("lodash")
+import { Worker } from "./Worker"
+import * as _ from "lodash";
+
 export class Manager
 {
     public main_worker: Worker | undefined;
+    public works: Worker[] | undefined;
 
-    constructor(_w: Worker | undefined){
-        if(! _.isUndefined(_w)){
+    constructor(_w: Worker | Worker[] | undefined){
+        if(! _.isUndefined(_w) && !_.isArray(_w)){
             this.main_worker = _w;
+        }
+        if(_.isArray(_w)){
+            this.works = _w;
         }
     }
 
@@ -15,18 +20,21 @@ export class Manager
         
     }
 
-    public set_main_worker(_w: any)
+    public set_main_worker(_w: Worker):Manager
     {
         this.main_worker = _w;
         return this;
     }
 
-    public get_main_worker()
+    public get_main_worker(): Worker
     {
+        if(_.isUndefined(this.main_worker)){
+            throw new Error("main_worker_have_not_set");
+        }
         return this.main_worker;
     }
 
-    public deliver_work_to(_m: any)
+    public deliver_work_to(_m: Manager)
     {
         return _m.set_main_worker(this.get_main_worker());
     }
