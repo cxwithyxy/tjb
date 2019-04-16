c70ca725220a5ce29dac9ad4c930a216419f0ea4
chenxuan <523764720@qq.com>
2019-04-16 10:54:40 +0800

    暂时停用登陆操作

--- work/Login_manager.ts
+++ work/Login_manager.ts
@@ -11,7 +11,7 @@ export class Login_manager extends Manager
     
     constructor(_w: Worker | undefined)
     {
-        super(_w);
+        super(_w)
     }
 
     init_work()
@@ -24,22 +24,44 @@ export class Login_manager extends Manager
                 webPreferences: {
                     nodeIntegration: false,
                     nodeIntegrationInWorker: false,
-                    contextIsolation: true
-                }
+                    contextIsolation: true,
+                    partition: "persist:tjb"
+                },
             }))
             .get_main_worker()
             .page_init()
             .open_dev()
             .set_ua("Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Mobile/15E148 Safari/604.1")
         }
-        return this.get_main_worker();
+        return this.get_main_worker()
     }
 
-    public async start() {
-        let ijh = new Inject_js_handler();
-        this.init_work();
+    async login_opera()
+    {
+        let limit = pLimit(1)
+        let queque_list: any[] = []
+        let while_seed = Math.random() * 15  + 5
+        while(while_seed > 0){
+            while_seed --
+            queque_list.push(limit(async () =>
+            {
+                this.get_main_worker().mouse_move(220 + Math.random() * 10, 420 + Math.random() * 20);
+                await sleep(Math.random() * 1000 * 0.04 + 0.01);
+            }))
+        }
+        await Promise.all(queque_list)
+         
+        this.get_main_worker().mouse_down(242, 433)
+        this.get_main_worker().mouse_up(242, 435)
+        
+        await sleep(1000)
+    }
+
+    async start() {
+        let ijh = new Inject_js_handler()
+        this.init_work()
         // .open_url("https://echo.opera.com")
-        this.get_main_worker().open_url("https://market.m.taobao.com/apps/market/tjb/core-member2.html");
+        this.get_main_worker().open_url("https://market.m.taobao.com/apps/market/tjb/core-member2.html")
 
         await this.get_main_worker().exec_js(ijh.to_code_string(
             `login_input_set(
@@ -48,23 +70,8 @@ export class Login_manager extends Manager
             )`
         ));
 
-        let limit = pLimit(1);
-        let queque_list: any[] = [];
-        let while_seed = Math.random() * 15  + 5;
-        while(while_seed > 0){
-            while_seed --;
-            queque_list.push(limit(async () =>
-            {
-                this.get_main_worker().mouse_move(220 + Math.random() * 10, 420 + Math.random() * 20);
-                await sleep(Math.random() * 1000 * 0.04 + 0.01);
-            }));
-        }
-        await Promise.all(queque_list);
+        // this.login_opera()
         
-        this.get_main_worker().mouse_down(242, 433);
-        this.get_main_worker().mouse_up(242, 435);
-
-        await sleep(1000);
 
     }
 }
