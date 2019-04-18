ed970a546efaf854f8f3d88cf52599b1fa83dc3a
chenxuan <523764720@qq.com>
2019-04-18 14:07:50 +0800

    引入定时任务，下午4点进行操作

--- index.ts
+++ index.ts
@@ -1,16 +1,21 @@
 import { app, BrowserWindow } from 'electron'
 import { Login_manager } from "./work/Login_manager"
 import { Qiang_jb_manager } from "./work/Qiang_jb_manager"
+import { scheduleJob } from "node-schedule";
 
 app.setPath("userData", app.getPath("temp") + "/tjb");
 app.on('ready', async () =>
 {
-    let M_login = new Login_manager()
-    
-    await M_login.start();
-    
-    let M_qiang_jb = new Qiang_jb_manager();
-    M_login.deliver_workers_to(M_qiang_jb);
-    await M_qiang_jb.start();
+
+    // let job = scheduleJob("0 6 14 * * *", async () =>
+    // {
+        let M_login = new Login_manager()
+        
+        await M_login.start();
+        
+        let M_qiang_jb = new Qiang_jb_manager();
+        M_login.deliver_workers_to(M_qiang_jb);
+        await M_qiang_jb.start();
+    // })
 });
 
