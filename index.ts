4d3549177ec8a4a8887f52d98712d08e8093305e
cx2889 <523764720@qq.com>
2019-04-21 01:03:15 +0800

    现在是先登录后再定时开始抢红包了

--- index.ts
+++ index.ts
@@ -7,14 +7,14 @@ app.setPath("userData", app.getPath("temp") + "/tjb");
 app.on('ready', async () =>
 {
 
-    let job = scheduleJob("50 59 15 * * *", async () =>
+    let M_login = new Login_manager()
+    
+    await M_login.start();
+    
+    let M_qiang_jb = new Qiang_jb_manager();
+    M_login.deliver_workers_to(M_qiang_jb);
+    let job = scheduleJob("40 59 0 * * *", async () =>
     {
-        let M_login = new Login_manager()
-        
-        await M_login.start();
-        
-        let M_qiang_jb = new Qiang_jb_manager();
-        M_login.deliver_workers_to(M_qiang_jb);
         await M_qiang_jb.start();
     })
 });
