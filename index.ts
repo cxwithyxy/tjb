98f2c88b34888fafe649529ff91da9490b2a81bf
cx2889 <523764720@qq.com>
2019-04-21 12:54:15 +0800

    协同测试命令行交互界面

--- index.ts
+++ index.ts
@@ -1,16 +1,19 @@
-import { app, BrowserWindow } from 'electron'
+import { app } from 'electron'
 import { Login_manager } from "./work/Login_manager"
 import { Zuo_renwu_manager } from "./work/Zuo_renwu_manager"
+import { UI } from "./electron_commandline_UI/commandline";
 
 app.setPath("userData", app.getPath("temp") + "/tjb");
 app.on('ready', async () =>
 {
-    let M_login = new Login_manager()
+    // let M_login = new Login_manager()
     
-    await M_login.start()
+    // await M_login.start()
     
-    let M_zuo_renwu = new Zuo_renwu_manager()
-    M_login.deliver_workers_to(M_zuo_renwu);
-    await M_zuo_renwu.start()
+    // let M_zuo_renwu = new Zuo_renwu_manager()
+    // M_login.deliver_workers_to(M_zuo_renwu);
+    // await M_zuo_renwu.start()
+
+    new UI()
 });
 
