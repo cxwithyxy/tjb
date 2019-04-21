a91f17f897cf7df03a1c447b9afa9dde115423a9
cxwithyxy <523764720@qq.com>
2019-04-21 16:28:28 +0800

    现在独立出负责显示界面的类， 并能够通过命令进行金币任务

--- index.ts
+++ index.ts
@@ -1,26 +1,9 @@
 import { app } from 'electron'
-import { Login_manager } from "./work/Login_manager"
-import { Zuo_renwu_manager } from "./work/Zuo_renwu_manager"
-import { UI, Handler } from "./electron_commandline_UI/commandline";
+import { Main_display } from './Main_display';
 
 app.setPath("userData", app.getPath("temp") + "/tjb");
 app.on('ready', async () =>
 {
-    // let M_login = new Login_manager()
-    
-    // await M_login.start()
-    
-    // let M_zuo_renwu = new Zuo_renwu_manager()
-    // M_login.deliver_workers_to(M_zuo_renwu);
-    // await M_zuo_renwu.start()
-
-    let my_ui = new UI()
-
-    await my_ui.init_win(`初始化成功`)
-    my_ui.on_msg((msg:any, handler?: Handler) =>
-    {
-        console.log(msg);
-        (<Handler>handler).send(`asdadasd`);
-    })
+    await new Main_display().display()
 });
 
