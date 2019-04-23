39940e4c85a8dd1fb539796b9e197a4d803fc35f
chenxuan <523764720@qq.com>
2019-04-23 11:19:09 +0800

    打算自己打开独立窗口控制触屏事件

--- work/Shou_cai_manager.ts
+++ work/Shou_cai_manager.ts
@@ -1,8 +1,6 @@
 import { Manager } from "./Manager"
 import { Worker } from "./Worker";
 import sleep from "sleep-promise";
-import forin_promise from "./../base/forin_promise";
-import { UI } from "electron_commandline_UI";
 
 
 export class Shou_cai_manager extends Manager
@@ -14,6 +12,18 @@ export class Shou_cai_manager extends Manager
     
     async start()
     {
+        this.set_main_worker(new Worker({ 
+            width: 480,
+            height: 800,
+            webPreferences: {
+                partition: "persist:tjb",
+                webSecurity: false
+            },
+        }))
+        .get_main_worker()
+        .page_init()
+        .set_ua("Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Mobile/15E148 Safari/604.1")
+
         await this.workers_do(async (_w) =>
         {
             _w.open_url(`https://taobao.com`)
